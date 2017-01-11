63b72de85483301bcea47afb97979d9c1a27e532
Nithramir <batiste.andre53@hotmail.fr>
2017-01-11 18:46:19 +0100

    creation graphe protégé, nettoyé, mais pas free

--- lem_in/src/create_graph.c
+++ lem_in/src/create_graph.c
@@ -12,46 +12,50 @@
 
 #include "lem_in.h"
 
+int		static split_not_valid_salle(char **split, int nb_split)
+{
+	int i;
+
+	i = 0;
+	if (split[0][0] == '#')
+		return (0);
+	while (split[i])
+		i++;
+	if (i != nb_split)
+		return (1);
+	return (0);
+}
+
 static int		parse_tubes(g_struct **list_salles, char *line)
 {
 	g_struct *salle1;
 	g_struct *salle2;
 	char **split;
 	int cont;
-	int i;
-	int j;
 
 	cont = 1;
 	split = NULL;
 	while (cont)
 	{
 	ft_putendl("yolo3");
-		split = ft_strsplit(line, '-');
+		if (!(split = ft_strsplit(line, '-')))
+			return (0);
+		if (split_not_valid_salle(split, 2))
+			return (1);
 		salle1 = graphchr(list_salles, split[0]);
 		salle2 = graphchr(list_salles, split[1]);
 		if (!salle1 || !salle2)
+			return (1);
+		if (!(salle1->liaisons = ptr_join(salle1->liaisons, salle2)))
 			return (0);
-		salle1->liaisons = ptr_join(salle1->liaisons, salle2);
-		salle2->liaisons = ptr_join(salle2->liaisons, salle1);
-	ft_putendl("yolo5");
+		if (!(salle2->liaisons = ptr_join(salle2->liaisons, salle1)))
+			return (0);
+		ft_putendl("yolo5");
+		free(line);
 		cont = get_next_line(0, &line);
 	}
-	i = 0;
 
 	return (1);
-}
-
-
-int		static split_not_valid_salle(char **split)
-{
-	int i;
-
-	i = 0;
-	while (split[i])
-		i++;
-	if (i != 1)
-		return (1);
-	return (0);
 }
 
 static g_struct	**parse_salles(g_struct **list_salles, int *start, int *end)
@@ -67,12 +71,14 @@ static g_struct	**parse_salles(g_struct **list_salles, int *start, int *end)
 			break;
 		if (!(split = ft_strsplit(line, ' ')))
 			return (NULL);
-		if (split_not_valid_salle(split))
+		if (split_not_valid_salle(split, 1))
 			return (list_salles);
 		if (!(ft_strcmp("##start", split[0])))
 			*start = i;
 		else if (!(ft_strcmp("##end", split[0])))
 			*end = i;
+		else if (split[0][0] == '#')
+			i++;
 		else
 		{
 			list_salles = graphe_join(list_salles, new_g_struct(split[0]));
@@ -81,12 +87,9 @@ static g_struct	**parse_salles(g_struct **list_salles, int *start, int *end)
 		free(line);
 	}
 	i = 0;
-	while (list_salles[i])
-	{
-		ft_putendl(list_salles[i]->name);
-		i++;
-	}
-	parse_tubes(list_salles, line);
+	afficher(list_salles);
+	if (parse_tubes(list_salles, line) == 0)
+		return (NULL);
 	return (list_salles);
 }
 
@@ -96,10 +99,17 @@ g_struct	**create_graph(g_struct **start, g_struct **end)
 	int			debut;
 	int			fin;
 
-	list_salles = (g_struct**)malloc(sizeof(g_struct*));
+	debut = -1;
+	fin = -1;
+	if (!(list_salles = (g_struct**)malloc(sizeof(g_struct*))))
+		return (NULL);
 	list_salles[0] = NULL;
 	list_salles = parse_salles(list_salles, &debut, &fin);
+	if (debut == fin || debut == -1 || fin == -1)
+		return (NULL);
 	*start = list_salles[debut];
 	*end = list_salles[fin];
+	if (!(*start) || !(*end))
+		return (NULL);
 	return (list_salles);
 }
