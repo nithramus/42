79d2eeafa73f08b06b03703c2074297babf1f939
Baptiste ANDRE <bandre@e3r7p1.42.fr>
2017-01-30 22:19:40 +0100

    gestion des coordonnee

--- lem_in/src/create_graph.c
+++ lem_in/src/create_graph.c
@@ -6,7 +6,7 @@
 /*   By: bandre <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/04 18:52:00 by bandre            #+#    #+#             */
-/*   Updated: 2017/01/15 23:25:20 by bandre           ###   ########.fr       */
+/*   Updated: 2017/01/30 22:19:01 by bandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int		static split_not_valid_salle(char **split, int nb_split)
 {
 	int i;
+	int j;
 
 	i = 0;
 	if (split[0][0] == '#')
@@ -23,6 +24,20 @@ int		static split_not_valid_salle(char **split, int nb_split)
 		i++;
 	if (i != nb_split)
 		return (1);
+	i = 1;
+	while (split[i])
+	{
+		j = 0;
+		while (split[i][j])
+		{
+			if (j == 0 && ft_strchr("+-", split[i][j]))
+				j++;
+			else if (ft_isdigit(split[i][j] == 0))
+				return (1);
+			j++;
+		}
+		i++;
+	}
 	return (0);
 }
 
@@ -67,6 +82,8 @@ static g_struct	**parse_salles(g_struct **list_salles, int *start, int *end)
 	{
 		if (!(split = ft_strsplit(line, ' ')))
 			return (NULL);
+		else if (!split[0])
+			return (list_salles);
 		else if (!(ft_strcmp("##start", split[0])))
 			*start = i;
 		else if (!(ft_strcmp("##end", split[0])))
@@ -75,7 +92,7 @@ static g_struct	**parse_salles(g_struct **list_salles, int *start, int *end)
 			i = i + 1 - 1;
 		else if (ft_strchr(line, '-'))
 			break;
-		else if (split_not_valid_salle(split, 1))
+		else if (split_not_valid_salle(split, 3))
 			return (list_salles);
 		else
 		{
