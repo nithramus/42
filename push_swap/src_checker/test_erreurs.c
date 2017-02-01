293e0fb0d7161a3bbd40425ef488a6457f114371
Baptiste ANDRE <bandre@e2r7p10.42.fr>
2017-02-01 16:46:17 +0100

    bonne gestion erreurs checker

--- push_swap/src_checker/test_erreurs.c
+++ push_swap/src_checker/test_erreurs.c
@@ -6,11 +6,11 @@
 /*   By: bandre <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 21:34:59 by bandre            #+#    #+#             */
-/*   Updated: 2017/02/01 15:12:17 by bandre           ###   ########.fr       */
+/*   Updated: 2017/02/01 16:45:53 by bandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "checker.h"
+#include "push_swap.h"
 
 static int	is_valide(char *value)
 {
@@ -38,6 +38,8 @@ int			*verif_entier(char *argv, int *ptr)
 {
 	ssize_t value;
 
+	if (ft_strlen(argv) > 14)
+		return (NULL);
 	if (is_valide(argv) == -1)
 		return (NULL);
 	value = ft_atoi(argv);
