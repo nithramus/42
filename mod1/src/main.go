eb18928bb71436c7ee3569de741133841b5c442c
nithramir <batiste.andre53@hotmail.fr>
2017-05-29 00:56:41 +0200

    ça a bosse les regex, et c'est pas mal

--- mod1/src/main.go
+++ mod1/src/main.go
@@ -36,8 +36,13 @@ func main() {
 	mode := flag.Int("mode", 1, "erreur dans le mode")
 	flag.Parse()
 	create_surface(&surface)
-	re := regexp.MustCompile("( *\\((?:[0-9]+,){2}[0-9]+\\))+")
-	fmt.Println("%q\n", re.FindString("(1500,1550,1555) (2000,2500,2550)"))
+	yolo := regexp.MustCompile("^(( *\\(([0-9]+,)([0-9]+,)([0-9]+)\\))+)$")
+	if yolo.MatchString("(1500,1550,1555)    (1,2,3)") {
+		fmt.Println(yolo.MatchString("(1500,1550,1555)"))
+	}
+	re := regexp.MustCompile(" *\\((?:([0-9]+),)(?:([0-9]+),)([0-9]+)\\)")
+	test := re.FindAllStringSubmatch("(1500,1550,1555) (2000,2500,2550) (1200,1250,125)", -1)
+	fmt.Println(test, "\nyolo :", test[1][1])
 	panic("")
 	if *mode < 1 || *mode > 3 {
 		fmt.Println("Seulement 3 mode, 1, 2 ou 3")
