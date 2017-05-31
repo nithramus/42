76028032ea3d3fbf09d605a440c6587a5746528f
nithramir <batiste.andre53@hotmail.fr>
2017-05-31 16:25:04 +0200

    modif

--- mod1/src/water_gen.go
+++ mod1/src/water_gen.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"math/rand"
 )
 
@@ -10,23 +10,77 @@ func water_gen(mode int, water *[width][height]block, surface *[width][height]fl
 		mode_normal(water, surface, hauteur)
 	}
 	if mode == 2 {
-		if int(hauteur)%10 == 0 {
-			for x := range water {
-				water[0][x].block += 1
+		if int(hauteur)%200 == 0 {
+			for x := range water[1] {
+				water[1][x].block += 1
 			}
 		}
-		mode_vague(water, surface)
+		mode_pluie(water, surface)
 	}
 	if mode == 3 {
 		x := rand.Intn(int(width))
 		y := rand.Intn(int(height))
 		water[x][y].block += 1
-		mode_vague(water, surface)
+		mode_pluie(water, surface)
 	}
+}
+func find_min(tab [4]float64) int {
+	var i int = 0
+	var min float64 = tab[0]
+	for j := 1; j < 4; j++ {
+		if min > tab[j] {
+			min = tab[j]
+			i = j
+		}
+	}
+	return i
 }
 
 func mode_pluie(water *[width][height]block, surface *[width][height]float64) {
-	fmt.Println("non utile")
+	var i int
+	var j int
+	var tab [4]float64
+	i = rand.Intn(2) + 1
+	for i < int(width-1) {
+		j = rand.Intn(2) + 1
+		for j < int(height-1) {
+			if int16(water[i][j].block) > water[i][j].height {
+				taille := float64(water[i][j].block)/2 + float64(surface[i][j])
+				tab[0] = float64(water[i+1][j].block)/2 + float64(surface[i+1][j]) - taille
+				tab[1] = float64(water[i][j+1].block)/2 + float64(surface[i][j+1]) - taille
+				tab[2] = float64(water[i][j-1].block)/2 + float64(surface[i][j-1]) - taille
+
+				tab[3] = float64(water[i-1][j].block) + float64(surface[i-1][j]) - taille
+
+				min := find_min(tab)
+				if tab[min] < 0 {
+					if min == 0 {
+						water[i][j].block -= 1
+						water[i+1][j].block += 1
+					} else if min == 1 {
+						water[i][j].block -= 1
+						water[i][j+1].block += 1
+
+					} else if min == 2 {
+						water[i][j].block -= 1
+						water[i][j-1].block += 1
+
+					} else if min == 3 {
+						water[i][j].block -= 1
+						water[i-1][j].block += 1
+					}
+				}
+
+			}
+			if water[i][j].height != int16(water[i][j].block) {
+				water[i][j].height = int16(water[i][j].block)
+				water[i][j].todraw = 1
+			}
+			j += 2
+		}
+		i += 2
+	}
+
 }
 
 func mode_normal(water *[width][height]block, surface *[width][height]float64, hauteur float64) {
@@ -45,9 +99,8 @@ func mode_normal(water *[width][height]block, surface *[width][height]float64, h
 func mode_vague(water *[width][height]block, surface *[width][height]float64) {
 	var i int
 	var j int
-	i = rand.Intn(2)
 	for i < int(width) {
-		j = rand.Intn(2)
+		j = 0
 		for j < int(height) {
 			if int16(water[i][j].block) > water[i][j].height {
 				taille := float64(water[i][j].block) + float64(surface[i][j]) - 1
@@ -72,9 +125,9 @@ func mode_vague(water *[width][height]block, surface *[width][height]float64) {
 					water[i][j].todraw = 1
 				}
 			}
-			j += 2
+			j++
 		}
 
-		i += 2
+		i++
 	}
 }
