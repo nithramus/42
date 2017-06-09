30aaa4c478f5e34f9332d852602552d81f26e769
nithramir <batiste.andre53@hotmail.fr>
2017-06-09 13:26:27 +0200

    on sur les bonus maintenant

--- capitaldata/main.go
+++ capitaldata/main.go
@@ -10,6 +10,7 @@ import (
 	//"os"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"reflect"
 )
 
 func read_file(name string) string {
@@ -20,17 +21,40 @@ func read_file(name string) string {
 	return string(data)
 }
 
-func init_database() {
-	db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/mysql")
+func init_database() *sql.DB {
+	db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/test")
 	if err != nil {
 		log.Fatal(err)
 	}
-	rows, err := db.Query("use test")
+	fmt.Println("Connected to database")
+	rows, err := db.Query("CREATE TABLE if not exists csv_file(first_name CHAR(50), last_name CHAR(50), email CHAR(100), date DATETIME);")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(rows)
+	fmt.Println("table created")
+	defer rows.Close()
+	return db
+}
 
+func complete_database(db *sql.DB, file [][]string) {
+	for i := range file {
+		request := "INSERT INTO csv_file VALUES("
+		for j := range file[i] {
+			if j < 3 {
+				request += "'" + file[i][j] + "'" + ","
+			} else {
+				file[i][j] = strings.Replace(file[i][j], "T", " ", 1)
+				file[i][j] = strings.Replace(file[i][j], "Z", "", 1)
+				request += "'" + file[i][j] + "'" + ")"
+			}
+		}
+		fmt.Println(request)
+		rows, err := db.Query(request)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+	}
 }
 
 func main() {
@@ -41,8 +65,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	init_database()
+	fmt.Println(reflect.TypeOf(records))
+	db := init_database()
 	fmt.Println(records)
+	complete_database(db, records)
+	db.Close()
 	/*	for i := range records {
 		for j := range records[i] {
 			fmt.Println(records[i][j])
